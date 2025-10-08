aae5937623881931b382a8935b6bd093cb4ceda4
박영민 <omin@omin-MacBook-Pro.local>
2025-10-08 18:38:16 +0900

    Add dapp-ui app

---
+++ dapp-ui/app/features/home/page/home.tsx
@@ -0,0 +1,14 @@
+import { supabase } from "~/postgres/supaclient"
+
+// before rendering Home react component(backend에서 실생)
+export async function loader() {
+    const { data } = await supabase().from("dapp-test").select("*");
+
+    console.log(data);
+}
+
+export default function Home() {
+    return <div>
+        Hello destat world
+    </div>
+}
